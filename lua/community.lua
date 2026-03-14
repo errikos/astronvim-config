f1fbccd91f340ba3346d3943c699624e3079fb1e
Ergys Dona <5298184+errikos@users.noreply.github.com>
2026-03-14 09:09:38 +0100

    Switch colorscheme to gruvbox

--- lua/community.lua
+++ lua/community.lua
@@ -7,4 +7,5 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
 }
